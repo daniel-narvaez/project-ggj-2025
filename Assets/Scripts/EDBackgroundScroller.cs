dd97d5692510ec1937a2b10215c5a8f8eed7146e
Kyle Yang <112425182+kyleyang2020@users.noreply.github.com>
2025-01-25 15:01:30 -0500

    added more prefabs to the spawner, now spawn at random rotations, started creation of gamestatemanager

--- Assets/Scripts/EDBackgroundScroller.cs
+++ Assets/Scripts/EDBackgroundScroller.cs
@@ -1,11 +1,14 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EDBackgroundScroller : MonoBehaviour
 {
     public float scrollSpeed = 2f; // Speed at which the background moves
-    public float resetPositionY = -10f; // The Y position where the background resets to create the looping effect
-    public float startPositionY = 10f; // The initial Y position of the background model
+    public float resetPositionY; // The Y position where the background resets to create the looping effect
+    public float startPositionY; // The initial Y position of the background model
     private Vector3 startPosition; // Starting position of the background
+
+    public bool dontReset;
 
     private void Start()
     {
@@ -19,8 +22,12 @@ public class EDBackgroundScroller : MonoBehaviour
         // Move the background along the Y-axis (or Z-axis, based on your scene)
         transform.Translate(Vector3.down * scrollSpeed * Time.deltaTime);
 
+        if (dontReset) 
+        { // nothing
+        }
+
         // If the background has moved past the reset position, reset it to create infinite scrolling
-        if (transform.position.y <= resetPositionY)
+        else if (transform.position.y <= resetPositionY)
         {
             // Reset the background position to the top (or depending on your setup, you can also reset the Z axis)
             transform.position = new Vector3(startPosition.x, startPositionY, startPosition.z);
